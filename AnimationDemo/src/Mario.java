f233b902bbb3af4a2220af555f1d96198c621532
ksrinivas788 <ksrinivas788@PR30.hhs.win.fuhsd.org>
2018-05-02 14:11:59 -0700

    jump()

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,6 +9,7 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
+	private double velocity = 0, acceleration = -9.81;
 
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
@@ -16,24 +17,24 @@ public class Mario extends Sprite {
 
 	// METHODS
 	public void walk(int dir) {
-	x += dir;
+	x += dir*5;
 		// WALK!
 	}
 
 	public void jump() {
-	y -= 10;	
+		for (double i = 0; i < 1; i+=0.01) {
+			y += (velocity*i + 0.5*acceleration*i*i);
+		}
 		// JUMP!
 	}
 
 	public void act(ArrayList<Shape> obstacles) {
-
 		boolean touching = false;
 		for (Shape s : obstacles) {
 			touching = ((s.intersects(this)));
 		}
-		System.out.println(touching);
 		if (!touching) {
-			y += 1;
+			y += 2;
 		}
 		// FALL (and stop when a platform is hit)
 	}
